f5219b42dbbc102bd0301a749282e34d3deb679e
artem alexeenko <artyom.alexeyenko@gmail.com>
2024-02-05 17:57:25 +0600

    add redux store, github api, models

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSearchUsersQuery } from '../store/github/github.api'
 
 function HomePage() {
+  const {isLoading, isError, data} = useSearchUsersQuery('mkdir');
+
+  console.log(data)
+
   return (
-    <div>HomePage</div>
+    <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+      {isError && <p className='text-center text-red-600'>Something went wrong...</p>}
+
+      
+    </div>
   )
 }
 
